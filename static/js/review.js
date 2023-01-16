ea9bf5682edfad58de073e9643ffeff62c5da128
jan <samjan1127@gmail.com>
2023-01-16 21:48:29 +0900

    refactor: 리뷰 js 코드 임시 주석 처리

--- static/js/review.js
+++ static/js/review.js
@@ -7,25 +7,25 @@ function showReviews() {
         success: function (response) {
             const rows = response['reviewList'];
 
-            // html 틀짜서 넣기
-            for (let i = 0; i < rows.length; i++) {
-                // for (let j = 0; j < rows[i]['rank'])
-                const tempHtml = `<div class="card w-50 mb-3">
-                                    <div class="card-body">
-                                        <h5 class="card-title">${rows[i]['member_name']}</h5>
-                                        <p class="card-text">${rows[i]['content']}</p>
-                                        <span class="star">
-                                            <img src="../static/image/star.png">
-                                            <img src="../static/image/star.png">
-                                            <img src="../static/image/star.png">
-                                            <img src="../static/image/star.png">
-                                            <img src="../static/image/star.png">
-                                        </span>
-                                    </div>
-                                </div>`;
-
-                $('#container').append(tempHtml);
-            }
+            // // html 틀짜서 넣기
+            // for (let i = 0; i < rows.length; i++) {
+            //     // for (let j = 0; j < rows[i]['rank'])
+            //     const tempHtml = `<div class="card w-50 mb-3">
+            //                         <div class="card-body">
+            //                             <h5 class="card-title">${rows[i]['member_name']}</h5>
+            //                             <p class="card-text">${rows[i]['content']}</p>
+            //                             <span class="star">
+            //                                 <img src="../static/image/star.png">
+            //                                 <img src="../static/image/star.png">
+            //                                 <img src="../static/image/star.png">
+            //                                 <img src="../static/image/star.png">
+            //                                 <img src="../static/image/star.png">
+            //                             </span>
+            //                         </div>
+            //                     </div>`;
+            //
+            //     $('#container').append(tempHtml);
+            // }
         }
     });
 }
