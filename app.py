3bc624c46417eb4a6671827d8c0072e50a78942f
YEONKYOUNG PARK <yyeon111@naver.com>
2023-01-16 23:07:59 +0900

    trainer 에러

--- app.py
+++ app.py
@@ -1,66 +1,44 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
-
-from dotenv import load_dotenv
-
-from datetime import datetime
-
-import os
-
-load_dotenv() # 환경변수 불러오기
-
 from pymongo import MongoClient
-client = MongoClient(os.getenv('DB_URL'))
+client = MongoClient('mongodb+srv://test:sparta@cluster0.s6i4xlw.mongodb.net/cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 리뷰 목록 불러오기
-@app.route('/api/trainer/review/<int:key>', methods=['GET'])
-def get_reviews(key):
-    all_review_key = db.trainer.find_one({'key': int(key)}, {'_id':False})['review_key']
+# @app.route("/api/trainer/check", methods = ["GET"])
+# def trainer_get():
+#     trainer_list = list(db.health_in.find({}))
+#     return jsonify({'trainer_list': trainer_list})
 
-    review_list = []
-
-    for review_key in all_review_key:
-        review = db.review.find_one({'key': review_key}, {'_id':False})
-        review_list.append(review)
-
-        member = db.member .find_one({'key': review['member_key']}, {'_id':False})
-        review_list[-1]['member_name'] = member['nickname']
-
-    return jsonify({'reviewList': review_list})
-
-# 리뷰 저장
-@app.route('/api/trainer/review/<int:key>', methods=['POST'])
-def set_review(key):
-    member_key = int(request.form['member_key'])
-    rank = int(request.form['rank'])
-    content = request.form['content']
-
-    db.trainer.update_one({'key': key}, {'$push': {'member_key': member_key}})
-
-    review_key = 1
-    review_list = list(db.review.find({}, {'_id': False}))
-
-    if len(review_list) != 0:
-        review_key = review_list[-1]['key'] + 1
+@app.route("/api/trainer/register", methods = ["POST"])
+def trainer_post():
+    name_receive: request.form['name_give']
+    region_receive: request.form['region_give']
+    category_receive: request.form['category_give']
+    timetable_receive: request.form['timetable_give']
+    price_receive: request.form['price_give']
+    description_receive: request.form['description_give']
 
     doc = {
-        'key': review_key,
-        'member_key': member_key,
-        'rank': rank,
-        'content': content,
-        'created_at': datetime.now().replace(microsecond=0)
+        'key':0,
+        'trainer_id': 'trainer1',
+        'password':'abcd1234',
+        'name':name_receive,
+        'region':region_receive,
+        'category' : category_receive,
+        'timetable':timetable_receive,
+        'price': price_receive,
+        'description':description_receive
     }
 
-    db.review.insert_one(doc)
 
-    return jsonify({'msg': '리뷰 작성 완료'})
+    return jsonify({'msg':'등록완료!'})
+
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=os.getenv('PORT'), debug=True)
+   app.run('0.0.0.0',port=5000,debug=True)
